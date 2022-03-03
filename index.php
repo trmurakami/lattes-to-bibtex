f3dde9260e168b8d3fb6c02f09a673200e661f1f
Tiago Murakami <trmurakami@gmail.com>
2022-03-03 16:15:48 -0300

    initial page

--- index.php
+++ index.php
@@ -1,22 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Conversor de XML do Currículo Lattes em formato Bibtex para importação no ORCID</title>
+  </head>
+  <body>
+
+    <div class="px-4 py-5 my-5 text-center">
+    <h1 class="display-5 fw-bold">Conversor de XML do Currículo Lattes em formato Bibtex para importação no ORCID</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Funcionando inicialmente com os tipos: Trabalhos em eventos, Artigos, Livros e Capítulos de Livros.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <form class="m-3" action="upload.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+            <legend>Inserir um XML do Lattes</legend>
+            <div class="input-group">
+                <input type="file" class="custom-file-input" id="fileXML" aria-describedby="fileXML" name="file">
+                <button class="btn btn-primary" type="submit">Enviar XML</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+
+
+
 <html>
 
 <body>
 
 
-    <form class="m-3" action="upload.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
-        <legend>Inserir um XML do Lattes</legend>
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text">XML Lattes</span>
-            </div>
-            <div class="custom-file">
-                <input type="file" class="custom-file-input" id="fileXML" aria-describedby="fileXML" name="file">
-                <label class="custom-file-label" for="fileXML">Escolha o arquivo</label>
-            </div>
-        </div>
-        <div class="input-group-append">
-            <button class="btn btn-primary" type="submit">Enviar XML</button>
-        </div>
-    </form>
+
 
 </body>
--- upload.php
+++ upload.php
@@ -213,6 +213,262 @@ if ($_FILES['file']['size'] != 0) {
         }
     }
 
+    if (isset($curriculo->{'PRODUCAO-BIBLIOGRAFICA'}->{'LIVROS-E-CAPITULOS'})) {
+
+        if (isset($curriculo->{'PRODUCAO-BIBLIOGRAFICA'}->{'LIVROS-E-CAPITULOS'}->{'LIVROS-PUBLICADOS-OU-ORGANIZADOS'})) {
+    
+            $livrosPublicadoArray = $curriculo->{'PRODUCAO-BIBLIOGRAFICA'}->{'LIVROS-E-CAPITULOS'}->{'LIVROS-PUBLICADOS-OU-ORGANIZADOS'}->{'LIVRO-PUBLICADO-OU-ORGANIZADO'};
+            foreach ($livrosPublicadoArray as $obra) {
+                $obra = get_object_vars($obra);
+                $dadosBasicosDoTrabalho = get_object_vars($obra["DADOS-BASICOS-DO-LIVRO"]);
+                $detalhamentoDoTrabalho = get_object_vars($obra["DETALHAMENTO-DO-LIVRO"]);
+
+                if (!empty($dadosBasicosDoTrabalho['@attributes']["TITULO-DO-LIVRO"])) {
+                    $recordContent[] = 'title   = {'.$dadosBasicosDoTrabalho['@attributes']["TITULO-DO-LIVRO"].'}';
+                }
+    
+                if (!empty($dadosBasicosDoTrabalho['@attributes']["ANO"])) {
+                    $recordContent[] = 'year = {'.$dadosBasicosDoTrabalho['@attributes']["ANO"].'}';
+                }
+    
+                if (!empty($dadosBasicosDoTrabalho['@attributes']["DOI"])) {
+                    $recordContent[] = 'doi = {'.$dadosBasicosDoTrabalho['@attributes']["DOI"].'}';
+                }
+    
+    
+                $sha256 = hash('sha256', ''.implode("", $recordContent).'');
+    
+                $record[] = '@book{book'.substr($sha256, 0, 8).',';
+                $record[] = implode(",\n", $recordContent);
+                $record[] = '}';
+    
+                $records_array[] = implode("\\n", $record);
+    
+                unset($recordContent);
+
+
+                // $doc["doc"]["type"] = "Work";
+                // $doc["doc"]["tipo"] = "Livro publicado ou organizado";
+                // $doc["doc"]["source"] = "Base Lattes";
+                // $doc["doc"]["lattes_ids"][] = (string)$curriculo->attributes()->{'NUMERO-IDENTIFICADOR'};
+                // if (isset($_REQUEST['tag'])) {
+                //     $doc["doc"]["tag"][] = $_REQUEST['tag'];
+                // } else {
+                //     $doc["doc"]["tag"][] = "Lattes";
+                // }
+                // $doc["doc"]["lattes"]["tipo"] = $dadosBasicosDoTrabalho['@attributes']["TIPO"];
+                // $doc["doc"]["datePublished"] = $dadosBasicosDoTrabalho['@attributes']["ANO"];
+                // $doc["doc"]["name"] = $dadosBasicosDoTrabalho['@attributes']["TITULO-DO-LIVRO"];
+                // $doc["doc"]["lattes"]["natureza"] = $dadosBasicosDoTrabalho['@attributes']['NATUREZA'];
+                // $doc["doc"]["country"] = $dadosBasicosDoTrabalho['@attributes']["PAIS-DE-PUBLICACAO"];
+                // $doc["doc"]["language"] = $dadosBasicosDoTrabalho['@attributes']["IDIOMA"];
+                // $doc["doc"]["lattes"]["meioDeDivulgacao"] = $dadosBasicosDoTrabalho['@attributes']["MEIO-DE-DIVULGACAO"];
+                // $doc["doc"]["url"] = $dadosBasicosDoTrabalho['@attributes']["HOME-PAGE-DO-TRABALHO"];
+                // $doc["doc"]["lattes"]["flagRelevancia"] = $dadosBasicosDoTrabalho['@attributes']["FLAG-RELEVANCIA"];
+                // $testDOI = Testadores::testDOI($dadosBasicosDoTrabalho['@attributes']["DOI"]);
+                // if ($testDOI === 1) {
+                //     $doc["doc"]["doi"] = $dadosBasicosDoTrabalho['@attributes']["DOI"];
+                // }
+                // $doc["doc"]["alternateName"] = $dadosBasicosDoTrabalho['@attributes']["TITULO-DO-LIVRO-INGLES"];
+                // $doc["doc"]["lattes"]["flagDivulgacaoCientifica"] = $dadosBasicosDoTrabalho['@attributes']["FLAG-DIVULGACAO-CIENTIFICA"];
+    
+                // $doc["doc"]["detalhamentoDoLivro"]["numeroDeVolumes"] = $detalhamentoDoTrabalho['@attributes']["NUMERO-DE-VOLUMES"];
+                // $doc["doc"]["numberOfPages"] = $detalhamentoDoTrabalho['@attributes']["NUMERO-DE-PAGINAS"];
+                // $doc["doc"]["isbn"] = $detalhamentoDoTrabalho['@attributes']["ISBN"];
+                // $doc["doc"]["bookEdition"] = $detalhamentoDoTrabalho['@attributes']["NUMERO-DA-EDICAO-REVISAO"];
+                // $doc["doc"]["detalhamentoDoLivro"]["numeroDaSerie"] = $detalhamentoDoTrabalho['@attributes']["NUMERO-DA-SERIE"];
+                // $doc["doc"]["publisher"]["organization"]["location"] = $detalhamentoDoTrabalho['@attributes']["CIDADE-DA-EDITORA"];
+                // $doc["doc"]["publisher"]["organization"]["name"] = $detalhamentoDoTrabalho['@attributes']["NOME-DA-EDITORA"];
+    
+                // if (!empty($obra["AUTORES"])) {
+                //     $array_result = processaAutoresLattes($obra["AUTORES"]);
+                //     $doc = array_merge_recursive($doc, $array_result);
+                // }
+    
+                // if (isset($obra["PALAVRAS-CHAVE"])) {
+                //     $array_result_pc = processaPalavrasChaveLattes($obra["PALAVRAS-CHAVE"]);
+                //     if (isset($array_result_pc)) {
+                //         $doc = array_merge_recursive($doc, $array_result_pc);
+                //     }
+                //     unset($array_result_pc);
+                // }
+    
+                // if (isset($obra["AREAS-DO-CONHECIMENTO"])) {
+                //     $array_result_ac = processaAreaDoConhecimentoLattes($obra["AREAS-DO-CONHECIMENTO"]);
+                //     if (isset($array_result_ac)) {
+                //         $doc = array_merge_recursive($doc, $array_result_ac);
+                //     }
+                //     unset($array_result_ac);
+                // }
+    
+                // // Vinculo
+                // $doc["doc"]["vinculo"] = construct_vinculo($_REQUEST, $curriculo);
+    
+                // // Constroi sha256
+                // if (!empty($doc["doc"]["doi"])) {
+                //     $sha256 = hash('sha256', ''.$doc["doc"]["doi"].'');
+                // } elseif (!empty($doc["doc"]["isbn"])) {
+                //     $sha_array[] = $doc["doc"]["lattes_ids"][0];
+                //     $sha_array[] = $doc["doc"]["isbn"];
+                //     $sha256 = hash('sha256', ''.implode("", $sha_array).'');
+                // } else {
+                //     $sha_array[] = $doc["doc"]["lattes_ids"][0];
+                //     $sha_array[] = $doc["doc"]["tipo"];
+                //     $sha_array[] = $doc["doc"]["name"];
+                //     $sha_array[] = $doc["doc"]["datePublished"];
+                //     $sha_array[] = $doc["doc"]["bookEdition"];
+                //     $sha256 = hash('sha256', ''.implode("", $sha_array).'');
+                // }
+    
+    
+                // //$doc["doc"]["bdpi"] = DadosExternos::query_bdpi_index($doc["doc"]["name"], $doc["doc"]["datePublished"]);
+                // $doc["doc"]["concluido"] = "Não";
+                // $doc["doc_as_upsert"] = true;
+    
+                // // Comparador
+                // $resultado = upsert($doc, $sha256);
+                // echo "<br/>";
+                // print_r($resultado);
+                // echo "<br/><br/>";
+    
+    
+                // unset($dadosBasicosDoTrabalho);
+                // unset($detalhamentoDoTrabalho);
+                // unset($obra);
+                // unset($doc);
+                // unset($sha_array);
+                // unset($sha256);
+                // flush();
+    
+            }
+        }
+    
+        if (isset($curriculo->{'PRODUCAO-BIBLIOGRAFICA'}->{'LIVROS-E-CAPITULOS'}->{'CAPITULOS-DE-LIVROS-PUBLICADOS'})) {
+    
+            $capitulosPublicadoArray = $curriculo->{'PRODUCAO-BIBLIOGRAFICA'}->{'LIVROS-E-CAPITULOS'}->{'CAPITULOS-DE-LIVROS-PUBLICADOS'}->{'CAPITULO-DE-LIVRO-PUBLICADO'};
+            foreach ($capitulosPublicadoArray as $obra) {
+                $obra = get_object_vars($obra);
+                $dadosBasicosDoTrabalho = get_object_vars($obra["DADOS-BASICOS-DO-CAPITULO"]);
+                $detalhamentoDoTrabalho = get_object_vars($obra["DETALHAMENTO-DO-CAPITULO"]);
+
+                if (!empty($dadosBasicosDoTrabalho['@attributes']["TITULO-DO-CAPITULO-DO-LIVRO"])) {
+                    $recordContent[] = 'title   = {'.$dadosBasicosDoTrabalho['@attributes']["TITULO-DO-CAPITULO-DO-LIVRO"].'}';
+                }
+    
+                if (!empty($dadosBasicosDoTrabalho['@attributes']["ANO"])) {
+                    $recordContent[] = 'year = {'.$dadosBasicosDoTrabalho['@attributes']["ANO"].'}';
+                }
+    
+                if (!empty($dadosBasicosDoTrabalho['@attributes']["DOI"])) {
+                    $recordContent[] = 'doi = {'.$dadosBasicosDoTrabalho['@attributes']["DOI"].'}';
+                }
+    
+    
+                $sha256 = hash('sha256', ''.implode("", $recordContent).'');
+    
+                $record[] = '@inbook{inbook'.substr($sha256, 0, 8).',';
+                $record[] = implode(",\n", $recordContent);
+                $record[] = '}';
+    
+                $records_array[] = implode("\\n", $record);
+    
+                unset($recordContent);
+    
+                // $doc["doc"]["type"] = "Work";
+                // $doc["doc"]["tipo"] = "Capítulo de livro publicado";
+                // $doc["doc"]["source"] = "Base Lattes";
+                // $doc["doc"]["lattes_ids"][] = (string)$curriculo->attributes()->{'NUMERO-IDENTIFICADOR'};
+                // if (isset($_REQUEST['tag'])) {
+                //     $doc["doc"]["tag"][] = $_REQUEST['tag'];
+                // } else {
+                //     $doc["doc"]["tag"][] = "Lattes";
+                // }
+                // $doc["doc"]["lattes"]["tipo"] = $dadosBasicosDoTrabalho['@attributes']["TIPO"];
+                // $doc["doc"]["name"] = $dadosBasicosDoTrabalho['@attributes']["TITULO-DO-CAPITULO-DO-LIVRO"];
+                // $doc["doc"]["datePublished"] = $dadosBasicosDoTrabalho['@attributes']["ANO"];
+                // $doc["doc"]["country"] = $dadosBasicosDoTrabalho['@attributes']["PAIS-DE-PUBLICACAO"];
+                // $doc["doc"]["language"] = $dadosBasicosDoTrabalho['@attributes']["IDIOMA"];
+                // $doc["doc"]["lattes"]["meioDeDivulgacao"] = $dadosBasicosDoTrabalho['@attributes']["MEIO-DE-DIVULGACAO"];
+                // $doc["doc"]["url"] = $dadosBasicosDoTrabalho['@attributes']["HOME-PAGE-DO-TRABALHO"];
+                // $doc["doc"]["lattes"]["flagRelevancia"] = $dadosBasicosDoTrabalho['@attributes']["FLAG-RELEVANCIA"];
+                // $testDOI = Testadores::testDOI($dadosBasicosDoTrabalho['@attributes']["DOI"]);
+                // if ($testDOI === 1) {
+                //     $doc["doc"]["doi"] = $dadosBasicosDoTrabalho['@attributes']["DOI"];
+                // }
+                // $doc["doc"]["alternateName"] = $dadosBasicosDoTrabalho['@attributes']["TITULO-DO-CAPITULO-DO-LIVRO-INGLES"];
+                // if (isset($dadosBasicosDoTrabalho['@attributes']["FLAG-DIVULGACAO-CIENTIFICA"])){
+                //     $doc["doc"]["lattes"]["flagDivulgacaoCientifica"] = $dadosBasicosDoTrabalho['@attributes']["FLAG-DIVULGACAO-CIENTIFICA"];
+                // }
+                // $doc["doc"]["isPartOf"]["name"] = $detalhamentoDoTrabalho['@attributes']["TITULO-DO-LIVRO"];
+                // $doc["doc"]["pageStart"] = $detalhamentoDoTrabalho['@attributes']["PAGINA-INICIAL"];
+                // $doc["doc"]["pageEnd"] = $detalhamentoDoTrabalho['@attributes']["PAGINA-FINAL"];
+                // $doc["doc"]["isPartOf"]["isbn"] = $detalhamentoDoTrabalho['@attributes']["ISBN"];
+                // $doc["doc"]["isPartOf"]["contributor"] = $detalhamentoDoTrabalho['@attributes']["ORGANIZADORES"];
+                // $doc["doc"]["bookEdition"] = $detalhamentoDoTrabalho['@attributes']["NUMERO-DA-EDICAO-REVISAO"];
+                // $doc["doc"]["serie"] = $detalhamentoDoTrabalho['@attributes']["NUMERO-DA-SERIE"];
+                // $doc["doc"]["publisher"]["organization"]["location"] = $detalhamentoDoTrabalho['@attributes']["CIDADE-DA-EDITORA"];
+                // $doc["doc"]["publisher"]["organization"]["name"] = $detalhamentoDoTrabalho['@attributes']["NOME-DA-EDITORA"];
+    
+                // if (!empty($obra["AUTORES"])) {
+                //     $array_result = processaAutoresLattes($obra["AUTORES"]);
+                //     $doc = array_merge_recursive($doc, $array_result);
+                // }
+    
+                // if (isset($obra["PALAVRAS-CHAVE"])) {
+                //     $array_result_pc = processaPalavrasChaveLattes($obra["PALAVRAS-CHAVE"]);
+                //     if (isset($array_result_pc)) {
+                //         $doc = array_merge_recursive($doc, $array_result_pc);
+                //     }
+                //     unset($array_result_pc);
+                // }
+    
+                // if (isset($obra["AREAS-DO-CONHECIMENTO"])) {
+                //     $array_result_ac = processaAreaDoConhecimentoLattes($obra["AREAS-DO-CONHECIMENTO"]);
+                //     if (isset($array_result_ac)) {
+                //         $doc = array_merge_recursive($doc, $array_result_ac);
+                //     }
+                //     unset($array_result_ac);
+                // }
+    
+                // // Vinculo
+                // $doc["doc"]["vinculo"] = construct_vinculo($_REQUEST, $curriculo);
+    
+                // // Constroi sha256
+                // if (!empty($doc["doc"]["doi"])) {
+                //     $sha256 = hash('sha256', ''.$doc["doc"]["doi"].'');
+                // } else {
+                //     $sha_array[] = $doc["doc"]["lattes_ids"][0];
+                //     $sha_array[] = $doc["doc"]["tipo"];
+                //     $sha_array[] = $doc["doc"]["name"];
+                //     $sha_array[] = $doc["doc"]["datePublished"];
+                //     $sha_array[] = $doc["doc"]["isPartOf"]["name"];
+                //     $sha256 = hash('sha256', ''.implode("", $sha_array).'');
+                // }
+    
+                // //$doc["doc"]["bdpi"] = DadosExternos::query_bdpi_index($doc["doc"]["name"], $doc["doc"]["datePublished"]);
+                // $doc["doc"]["concluido"] = "Não";
+                // $doc["doc_as_upsert"] = true;
+    
+                // // Comparador
+                // $resultado = upsert($doc, $sha256);
+                // echo "<br/>";
+                // print_r($resultado);
+                // echo "<br/><br/>";
+    
+    
+                // unset($dadosBasicosDoTrabalho);
+                // unset($detalhamentoDoTrabalho);
+                // unset($obra);
+                // unset($doc);
+                // unset($sha_array);
+                // unset($sha256);
+                // flush();
+    
+            }
+        }
+    
+    }
+
 
     $file="lattes.bib";
     header('Content-type: text/plain');
